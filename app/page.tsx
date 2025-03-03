0949053176f89363f6fe67a13e0f9175f9be407d
Kent Dang <kenttdang@gmail.com>
2025-03-03 01:13:15 -0500

    [chore]: 🧹 Removed unused files

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@
 import { SettingsProvider } from "@/lib/settings-provider";
 import { RealtimeAudio } from "@/components/screenpipe/realtime-audio";
 import Camera from "@/components/camera";
-import { useState } from "react";
 export default function Page() {
 
-  const [isTranscribing, setIsTranscribing] = useState<boolean>(false);
   return (
     <SettingsProvider>
       <RealtimeAudio />
